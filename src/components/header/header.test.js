a4dce46738655ff69b00bd446e5e115e14cb6512
Rahul Verma <rv@wandaguru.com>
2019-09-29 15:13:28 +0100

    Update: Header Component with Tests

---
+++ src/components/header/header.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../../../Utils';
+import Header from './index';
+
+const setUp = (props={}) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
+describe('Header Component', () => {
+
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+  
+  it('should render without errors', () => {
+    const wrapper = findByTestAttr(component, 'headerComponent');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('should render a logo', () => {
+    const logo = findByTestAttr(component, 'logoImage');
+    expect(logo.length).toBe(1);
+  });
+
+});
+
